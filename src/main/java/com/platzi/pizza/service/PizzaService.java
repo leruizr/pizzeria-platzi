278cc2e9e3489e9487354a62ed636795259a8e31
leruizr <leruizres1986@gmail.com>
2024-08-21 20:44:35 -0500

    se crea pizza service, controller y repository para realizar la consulta de las pizzas

---
+++ src/main/java/com/platzi/pizza/service/PizzaService.java
@@ -0,0 +1,29 @@
+package com.platzi.pizza.service;
+
+
+import com.platzi.pizza.persistence.entity.PizzaEntity;
+import com.platzi.pizza.persistence.repository.PizzaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PizzaService {
+    private final PizzaRepository pizzaRepository;
+
+    @Autowired
+    public PizzaService(PizzaRepository pizzaRepository) {
+        this.pizzaRepository = pizzaRepository;
+    }
+
+    public List<PizzaEntity> getAll() {
+        return this.pizzaRepository.findAll();
+    }
+
+    public PizzaEntity get(int idPizza) {
+        return this.pizzaRepository.findById(idPizza).orElse(null);
+    }
+}
