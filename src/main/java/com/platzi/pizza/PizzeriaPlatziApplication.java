509a38e74454b9a7a5ef027e1fa1d11062976c88
leruizr <leruizres1986@gmail.com>
2024-08-21 19:15:47 -0500

    creacion del proyecto

---
+++ src/main/java/com/platzi/pizza/PizzeriaPlatziApplication.java
@@ -0,0 +1,14 @@
+package com.platzi.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaPlatziApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PizzeriaPlatziApplication.class, args);
+	}
+
+}
