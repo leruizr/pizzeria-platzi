98104ad071d42553801c770320d6ac96031550d0
leruizr <leruizres1986@gmail.com>
2024-08-23 20:58:32 -0500

    se crea AuditableEntity

--- src/main/java/com/platzi/pizza/persistence/entity/PizzaEntity.java
+++ src/main/java/com/platzi/pizza/persistence/entity/PizzaEntity.java
@@ -1,21 +1,18 @@
 package com.platzi.pizza.persistence.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "pizza")
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @NoArgsConstructor
-public class PizzaEntity {
+public class PizzaEntity extends AuditableEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_pizza", nullable = false)
