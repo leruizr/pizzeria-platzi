f112a2f811b426c774c7b154abc067b5677e0e6c
leruizr <leruizres1986@gmail.com>
2024-08-27 11:46:25 -0500

    se crea la configuracion del CORS

--- src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 @Configuration
 public class SecurityConfig {
@@ -13,12 +14,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)  // Deshabilitar CSRF
+                .cors(Customizer.withDefaults()) // Habilitar CORS
                 .authorizeHttpRequests(customizeRequests -> {
                             customizeRequests
                                     .anyRequest()
                                     .authenticated();
                         }
-                ).httpBasic(Customizer.withDefaults());
+                )
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
