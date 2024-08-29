c9ee7de49d7866cb7befae597de4a29ce990accb
leruizr <leruizres1986@gmail.com>
2024-08-29 14:26:17 -0500

    se aplica filtro en la configuracion

--- src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.platzi.pizza.web.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,37 +10,48 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
+    private final JwtFilter jwtFilter;
+
+    @Autowired
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)  // Deshabilitar CSRF
-                .cors(Customizer.withDefaults()) // Habilitar CORS
-                .authorizeHttpRequests(customizeRequests -> {
-                            customizeRequests
-                                    .requestMatchers("/api/auth/**").permitAll()
-                                    .requestMatchers("/api/customers/**").hasAnyRole("CUSTOMER", "ADMIN")
-                                    .requestMatchers(HttpMethod.GET, "/api/pizzas/**").hasAnyRole("CUSTOMER", "ADMIN")
-                                    .requestMatchers(HttpMethod.POST, "/api/pizzas/**").hasRole("ADMIN")
-                                    .requestMatchers(HttpMethod.PUT).hasRole("ADMIN")
-                                    .requestMatchers("/api/orders/random").hasAuthority("random_order")
-                                    .requestMatchers("/api/orders/**").hasRole("ADMIN")
-                                    .anyRequest()
-                                    .authenticated();
-                        }
+                .cors(Customizer.withDefaults())  // Habilitar CORS
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)  // Deshabilitar sesiones
                 )
-                .httpBasic(Customizer.withDefaults());
+                .authorizeHttpRequests(customizeRequests -> customizeRequests
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/customers/**").hasAnyRole("CUSTOMER", "ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/pizzas/**").hasAnyRole("CUSTOMER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/pizzas/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/pizzas/**").hasRole("ADMIN")  // Especifica la URL para PUT
+                        .requestMatchers("/api/orders/random").hasAuthority("random_order")
+                        .requestMatchers("/api/orders/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);  // Agregar filtro JWT antes de UsernamePasswordAuthenticationFilter
 
         return http.build();
     }
+
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
