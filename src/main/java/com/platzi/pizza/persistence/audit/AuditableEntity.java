cd238a58de3a178a516ada3c2c8e9157fcf7658f
leruizr <leruizres1986@gmail.com>
2024-08-29 14:42:33 -0500

    auditoria de usuarios en la base de datos

--- src/main/java/com/platzi/pizza/persistence/audit/AuditableEntity.java
+++ src/main/java/com/platzi/pizza/persistence/audit/AuditableEntity.java
@@ -2,14 +2,15 @@ package com.platzi.pizza.persistence.audit;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 public class AuditableEntity {
-
     @Column(name = "created_date")
     @CreatedDate
     private LocalDateTime createdDate;
@@ -17,4 +18,12 @@ public class AuditableEntity {
     @Column(name = "modified_date")
     @LastModifiedDate
     private LocalDateTime modifiedDate;
+
+    @Column(name = "created_by")
+    @CreatedBy
+    private String createdBy;
+
+    @Column(name = "modified_by")
+    @LastModifiedBy
+    private String modifiedBy;
 }
